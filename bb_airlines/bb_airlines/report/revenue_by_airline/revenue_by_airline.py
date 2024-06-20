a30caed3f455a7a01a7739fcaa4d7e8469892f84
Victor Arocha <victor.arocha@gmail.com>
2024-06-20 01:37:34 +0000

    Assignment 3 before airplane audits

--- bb_airlines/bb_airlines/report/revenue_by_airline/revenue_by_airline.py
+++ bb_airlines/bb_airlines/report/revenue_by_airline/revenue_by_airline.py
@@ -2,19 +2,32 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.query_builder.functions import Count
 from frappe.query_builder.functions import Sum
 
 def execute(filters=None):
-	columns, data = [], []
+	columns, data, report_summary = [], [], []
 	airplane = frappe.qb.DocType('Airplane')
+	airline = frappe.qb.DocType('Airline')
 	ticket = frappe.qb.DocType('Airplane Ticket')
 	flight = frappe.qb.DocType('Airplane Flight')
-	revenue = Sum(ticket.total_amount).as_('revenue')
 
-	query = frappe.qb.select(airplane.airline, revenue).from_(ticket).join(flight).on(ticket.flight == flight.name).join(airplane).on(airplane.name == flight.airplane).groupby(airplane.airline)
+	query = (
+		frappe.qb
+		.select(airline.name.as_('airline'), Sum(ticket.total_amount).as_('revenue'))
+		.from_(airline)
+		.left_join(airplane).on(airline.name == airplane.airline)
+		.left_join(flight).on(airplane.name == flight.airplane)
+		.left_join(ticket).on(flight.name == ticket.flight)
+		.groupby(airline.name)
+	)
 
-	data = query.run()
+	data = query.run(as_dict=True)
+
+	for x in data:
+		if x.revenue is None:
+			x.revenue = 0
+
+	total_revenue = sum([x.revenue for x in data])
 
 	columns = [
 		{
@@ -34,4 +47,24 @@ def execute(filters=None):
 
 		}]
 
-	return columns, data
+	chart = {
+		"data": {
+			"labels": [x.airline for x in data],
+			"datasets": [
+				{
+					"values": [x.revenue for x in data]
+				}
+			],
+		},
+		"type": "donut"
+	}
+
+	report_summary = [{
+		"value": total_revenue,
+		"label": "Total Revenue",
+		"indicator": "Green",
+		"datatype": "Currency",
+		"currency": "USD"
+	}]
+
+	return columns, data, None, chart, report_summary
